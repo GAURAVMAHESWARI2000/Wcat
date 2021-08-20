d539ac233a4b08ce959d14c2dc8c1abc438df61f
gauravmaheswari2000 <gauravjimaheshwari@gmail.com>
2021-08-20 14:50:54 +0530

    added A,b,e,E,n flags of original cat command

--- index.js
+++ index.js
@@ -86,26 +86,26 @@ for(let file of filenames){
                 fileData = fileData.split(secondaryArgument).join("")
             }
         }
-        if(flag == "-s"){  //add numbering to each line in increasing order
+        if(flag == "-s" || flag =="-n"){  //add numbering to each line in increasing order
             let arr = fileData.split("\n")
             for(let i=0;i<arr.length;i++){
-                arr[i] = (i+1) +" " + arr[i] 
+                arr[i] = "     "+(i+1) +"  " + arr[i] 
             }
             fileData = arr.join("\n")
         }
-        if(flag == "-sn"){      ////add numbering to each non-empty line in increasing order
+        if(flag == "-sn" ||flag == "-b"){      ////add numbering to each non-empty line in increasing order
             let count =1;
             let arr = fileData.split("\n")
             for(let i=0;i<arr.length;i++){
                 if(arr[i]!=""){
-                    arr[i] = count +" " + arr[i] 
+                    arr[i] = "     "+count +"  " + arr[i] 
                     count++
                 }
                 
             }
             fileData = arr.join("\n")
         }
-        if(flag = "-rel"){   // remove extra empty lines when there are more than 1 empty lines keep only one empty line
+        if(flag == "-rel"){   // remove extra empty lines when there are more than 1 empty lines keep only one empty line
             let arr = fileData.split("\n")
             let data = []
             for(let i=0;i<arr.length;i++){
@@ -117,6 +117,13 @@ for(let file of filenames){
                 }
             }
             fileData = data.join("\n")
+        }
+        if(flag == "-A" || flag == "-e" || flag == "-E"){  //add $ to end of every  line same as cat -A command
+            let arr = fileData.split("\n")
+            for(let i=0;i<arr.length;i++){
+                    arr[i] = arr[i]+"$"
+            }
+            fileData = arr.join("\n")
         }
     }
     console.log(fileData)
